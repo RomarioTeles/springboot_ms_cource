39d912682e707ed9a059db0a912791ad0f22ec99
Romario Teles Pires <rpires@cpqi.com>
2020-11-12 23:57:29 -0300

    FeignClient

---
+++ hr-payroll/src/main/java/com/devsuperior/payroll/feignClient/WorkerFeignClient.java
@@ -0,0 +1,22 @@
+package com.devsuperior.payroll.feignClient;
+
+import java.util.List;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.devsuperior.payroll.entities.Worker;
+
+@Component
+@FeignClient(name = "hr-worker", url = "localhost:8001", path = "/workers")
+public interface WorkerFeignClient {
+
+	@GetMapping
+	ResponseEntity<List<Worker>> findAll();
+	
+	@GetMapping("/{id}")
+	ResponseEntity<Worker> findById(@PathVariable Long id);
+}
